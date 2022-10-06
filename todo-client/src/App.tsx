e4540fc88cd2585364b04e7664623c9704c74be6
mutuajoseph <wambuajoseph756@gmail.com>
2022-10-06 10:45:34 +0300

    Feat(frontend): Routing and UI library installation

--- todo-client/src/App.tsx
+++ todo-client/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './views/Home';
+import Login  from './views/Login';
+import Todo from './views/Todo';
+import 'dracula-ui/styles/dracula-ui.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/app/tasks' element={<Todo />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ todo-client/src/views/Login.tsx
@@ -0,0 +1,12 @@
+import { Badge } from 'dracula-ui'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login
+        <Badge color="animated">Fun Badge</Badge>;
+    </div>
+  )
+}
+
+export default Login
